1bb8e39381d83aca96e736cc119bd6fa5749f668
Hanole <Olemagnus_Stallvik@hotmail.no>
2025-04-14 23:09:37 +0200

    added goodreads button, gotta fix the button position

--- src/components/book-card/book-card.component.jsx
+++ src/components/book-card/book-card.component.jsx
@@ -1,8 +1,15 @@
 import './book-card.styles.scss';
 import Button from '../button/button.component';
+import { FaGoodreads } from 'react-icons/fa';
+
 
 const BookCard = ({ book }) => {
-    const { title, pages, imageUrl } = book;
+    const { title, pages, imageUrl, url } = book;
+
+    const openGoodreads = () => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+      };
+
     return (
     <article className='book-wrapper'>
     <div className='book-card-container'>
@@ -14,7 +21,12 @@ const BookCard = ({ book }) => {
             <p>{pages}</p>
         </div>
         <div className="bookbutton">
-            <Button buttonType='inverted'>GoodReads</Button>
+            <Button buttonType='inverted' onClick={openGoodreads}>
+            <span className='goodreads-container'>
+                <FaGoodreads style={{ fontSize: '1.5rem', color: '#6D4C41', marginRight: '8px' }} />
+            </span>
+            GoodReads
+            </Button>
         </div>
     </div>
     </article>
